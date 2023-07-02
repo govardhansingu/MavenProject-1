1d4280ec42f578980e6e3b0cfc6c974932605e33
govardhan.singu <govardhan.singu@gmail.com>
2023-07-03 00:05:58 +0530

    1st commit

---
+++ src/main/java/webserver/com/website/war/domain/link/package-info.java
@@ -0,0 +1,6 @@
+@XmlSchema (
+  namespace = "http://api.example.com/link"
+)
+package webserver.com.website.war.domain.link;
+
+import javax.xml.bind.annotation.XmlSchema;
---
+++ src/main/java/webserver/com/website/war/domain/persona/package-info.java
@@ -0,0 +1,6 @@
+@XmlSchema (
+  namespace = "http://api.example.com/persona"
+)
+package webserver.com.website.war.domain.persona;
+
+import javax.xml.bind.annotation.XmlSchema;
